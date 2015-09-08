27c9ccf51c2d3cd8a9b41fec681bb9d8cc30f627
Thomas Wendt <thoemy@gmx.net>
2015-09-09 00:52:43 +0200

    g620-a2: Fix Wifi tethering
    libqsap_sdk expects the file to be named hostapd_default.conf.
    
    Change-Id: Ic8be47eee0a0089f62564d8cee4edf3ff58d9438

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -22,7 +22,7 @@ LOCAL_SRC_FILES    := hostapd.deny
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := hostapd.conf
+LOCAL_MODULE       := hostapd_default.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
